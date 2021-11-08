b08866c59cdee945b93791051f761ec963d0200d
CODECZERO <84948522+CODECZERO@users.noreply.github.com>
2021-11-08 00:00:55 -0500

    Add files via upload
    Can't  decide what do today it will help you to decide what to do.

---
+++ random_choice.py
@@ -0,0 +1,7 @@
+import random
+#now time put list.Edit if you want and make it you way.
+list_choice=["hello","my name","1","23","CODEZERO","SupportME"]
+#List has been make.
+print(random.choice(list_choice))
+#you write is as random.choice(list_choice)
+#If you like support us and give feedback.This is Open source 
